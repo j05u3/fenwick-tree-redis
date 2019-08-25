3a169d6a3a01bc33e6fcc1477aca12cf7858b764
josue.0 <josueajc@gmail.com>
2019-08-25 02:26:23 -0500

    Improved comments

--- src/lib/backend.ts
+++ src/lib/backend.ts
@@ -1,8 +1,15 @@
 export type Int = number; // to be validated as Int in the future
 
+/**
+ * The backend must make the read and increase operations atomic. 
+ * This is to make sure the next operations are atomic (not executing parts of one in the middle of the other):
+ *    * update(x, v) 
+ *    * query(b) - query(a)
+ */
 export interface Backend {
   maximum: Int;
 
+  
   /**
    * The backend must make sure that the whole execution of these queries is atomic.
    * That is: we don't want an increase-operation to be executed sometime in the middle of
@@ -23,6 +30,10 @@ export class IncreaseQuery {
   val: number;
 }
 
+/**
+ * We use MULTI with different clients each time (on each read and increase operation) 
+ * because we want to ensure atomicity.
+ */
 export class RedisHashBackend implements Backend {
   maximum: number;
   redisCredentials: any;
@@ -32,10 +43,6 @@ export class RedisHashBackend implements Backend {
     this.maximum = maximum;
   }
 
-  // We use MULTI with different clients each time because we want to:
-  // * Be able to make the next operations atomic (not executing parts of one in the middle of the other):
-  //    * update(x, v) 
-  //    * query(b) - query(a)
   async read(queries: number[]): Promise<number[]> {
     if (queries.length == 0) return [];
     // TODO: build this wrapping all in a MULTI with a new redis client each time
